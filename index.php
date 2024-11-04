7a7251cb6dafe0a1f947a849cef41ef7e749ff10
Komiljonovdev <uzokdeveloper@gmail.com>
2024-11-04 19:58:49 +0500

    MINOR: added required of time

--- index.php
+++ index.php
@@ -6,65 +6,75 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Work Of Tracker</title>
-    <style>
-        table, tr, td, th {
-            border: solid black 2px;
-        }
-    </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
     <h1 class="text-danger text-center">Work Of Tracker</h1>
-    <form method="post">
-        <div class="mb-3">
-            <label for="name" class="form-label">Ism</label>
-            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name" required>
+    <div class="row align-items-end my-3">
+        <div class="col">
+            <form method="post" class="row g-3 mt-3 align-items-end">
+                <div class="col-auto">
+                    <label for="name">Ismi</label>
+                    <input type="text" name="name" class="form-control" id="name">
+                </div>
+                <div class="col-auto">
+                    <label for="arrived_at">Kelgan vaqti</label>
+                    <input type="datetime-local" name="arrived_at" class="form-control" id="arrived_at">
+                </div>
+                <div class="col-auto">
+                    <label for="leaved_at">Ketgan vaqti</label>
+                    <input type="datetime-local" name="leaved_at" class="form-control" id="leaved_at">
+                </div>
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary">Yuborish</button>
+                </div>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="arrived_at" class="form-label">Kelgan vaqt</label>
-            <input type="datetime-local" class="form-control" id="arrived_at" name="arrived_at">
-        </div>
-        <div class="mb-3">
-            <label for="leaved_at" class="form-label">Ketgan vaqt</label>
-            <input type="datetime-local" class="form-control" id="leaved_at" name="leaved_at">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
+
     <?php
+    const REQUIRED_WORK_HOUR_DAILY = 8;
 
     $dsn = 'mysql:host=127.0.0.1;dbname=work_of_tracker';
     $pdo = new PDO($dsn, 'root', '1234');
     if (isset($_POST['name']) && isset($_POST['arrived_at']) && isset($_POST['leaved_at'])) {
-        $name = $_POST['name'];
-        $arrived_at = $_POST['arrived_at'];
-        $leaved_at = $_POST['leaved_at'];
 
-        $query = "INSERT INTO daily (name,arrived_at,leaved_at) 
-                    VALUES (:name, :arrived_at, :leaved_at)";
-        $stmt = $pdo->prepare($query);
+        if (!empty($_POST['name']) && !empty($_POST['arrived_at']) && !empty($_POST['leaved_at'])) {
+            $name = $_POST['name'];
+            $arrived_at = new DateTime($_POST['arrived_at']);
+            $leaved_at = new DateTime($_POST['leaved_at']);
 
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':arrived_at', $arrived_at);
-        $stmt->bindParam(':leaved_at', $leaved_at);
-        $stmt->execute();
+            $diff = $arrived_at->diff($leaved_at);
+            $hour = $diff->h;
+            $minute = $diff->i;
+            $second = $diff->s;
+            $total = ((REQUIRED_WORK_HOUR_DAILY * 3600) - ($hour * 3600) + ($minute * 60));
+
+            $query = "INSERT INTO daily (name,arrived_at,leaved_at, required_of) 
+                        VALUES (:name, :arrived_at, :leaved_at, :required_of)";
+            $stmt = $pdo->prepare($query);
+
+            $stmt->bindParam(':name', $name);
+            $stmt->bindValue(':arrived_at', $arrived_at->format('Y-m-d H:i'));
+            $stmt->bindValue(':leaved_at', $leaved_at->format('Y-m-d H:i'));
+            $stmt->bindParam(':required_of', $total);
+            $stmt->execute();
+        }
     }
+
     $query = "SELECT * FROM daily";
     $stmt = $pdo->query($query);
     $records = $stmt->fetchAll();
     ?>
-
-</div>
-
-
-<div class="container">
-    <table class="table table-primary">
+    <table class="table table-primary table-hover">
         <thead>
         <tr class="table-secondary">
             <th scope="col">#</th>
             <th scope="col">Ism</th>
             <th scope="col">Kelgan vaqti</th>
             <th scope="col">Ketgan vaqti</th>
+            <th scope="col">Ishlash kerak</th>
         </tr>
         </thead>
         <tbody>
@@ -72,16 +82,15 @@
 
         foreach ($records as $record) {
             echo "<tr>
-                <th scope='row'>{$record['id']}</th>
+                <td>{$record['id']}</td>
                 <td>{$record['name']}</td>
                 <td>{$record['arrived_at']}</td>
                 <td>{$record['leaved_at']}</td>
+                <td>" . gmdate('H:i',$record['required_of']) . "</td>
             </tr>";
         }
 
         ?>
-
-
         </tbody>
     </table>
 </div>
